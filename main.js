89ffdb6fd9501b56029ce65583335134a058cc4e
Adam Smith <adamps724@gmail.com>
2023-08-30 20:53:28 -0500

    updating fetch request

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 
-document.querySelector('button').addEventListener('click', getQuote)
+// document.querySelector('button').addEventListener('click', getQuote)
 
- function getQuote(){
+//  function getQuote(){
 
-	fetch( "https://seinfeld-quotes-api-9e3f304f1eda.herokuapp.com/api/random")
-		.then(res => res.json()) 
-		.then(data => {
-			console.log (data);
-			document.querySelector('h2').innerText = data.quote;
-			document.querySelector('img').src = data.img;
-			document.querySelector('h3').innerText = data.name;
-		})
-		.catch(err=> {
-			console.log(`error ${err}`)
-		})
-  }		
+// 	fetch( "https://seinfeld-quotes-api.glitch.me/api/random")
+// 		.then(res => res.json()) 
+// 		.then(data => {
+// 			console.log (data);
+// 			document.querySelector('h2').innerText = data.quote;
+// 			document.querySelector('img').src = data.img;
+// 			document.querySelector('h3').innerText = data.name;
+// 		})
+// 		.catch(err=> {
+// 			console.log(`error ${err}`)
+// 		})
+//   }		
+
+document.querySelector('button').addEventListener('click', getQuote);
+
+async function getQuote() {
+  try {
+    const response = await fetch("https://seinfeld-quotes-api.onrender.com/api/random");
+    const data = await response.json();
+
+    console.log(data);
+    document.querySelector('h2').innerText = data.quote;
+    document.querySelector('img').src = data.img;
+    document.querySelector('h3').innerText = data.name;
+  } catch (err) {
+    console.log(`error ${err}`);
+  }
+}
