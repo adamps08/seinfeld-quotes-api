b75070c0d9bfa991d9975d1d89ae3e507fa97e76
Adam Smith <adamps724@gmail.com>
2023-09-03 16:24:43 -0500

    removed the auto refresh

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ async function addLike(){
   const currentAuthor = document.querySelector('h3').innerText;
   const currentLikes = Number(document.querySelector('.likeAmount').innerText)
   try{
-      const response = await fetch('https://seinfeld-quotes-api.cyclic.app/api/random/addOneLike', {
+      const response = await fetch('https://seinfeld-quotes-api.cyclic.app/addOneLike', {
           method: 'PUT',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -105,7 +105,7 @@ async function addLike(){
         })
       const data = await response.json()
       console.log(data)
-      location.reload()
+      // location.reload()
 
   }catch(err){
       console.log(err)
