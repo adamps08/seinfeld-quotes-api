e636f6850d3ac1ebb39de635cd1ba04efd0af509
Adam Smith <adamps724@gmail.com>
2023-09-05 20:06:15 -0500

    forgot to update /addOneLike url

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ async function addLike(){
     const currentLikesElement = document.querySelector('.likeAmount');
     let currentLikes = parseInt(currentLikesElement.innerText, 10)
   try{
-      const response = await fetch('http://localhost:8000/addOneLike', {
+      const response = await fetch('https://seinfeld-quotes-api.cyclic.app/addOneLike', {
           method: 'PUT',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
